c61b9f4d303a0428fa2a4f50f45d2e468e073680
VISHNU K <vishnuky16@gmail.com>
2022-08-25 22:01:54 +0530

    done

--- lib/app/home/viewmodel/infrast.dart
+++ lib/app/home/viewmodel/infrast.dart
@@ -2,18 +2,18 @@ import 'package:dio/dio.dart';
 import 'package:fake_api/app/home/model/model.dart';
 import 'package:flutter/cupertino.dart';
 
-class HomeProv extends ChangeNotifier{
+class HomeProv extends ChangeNotifier {
+  HomeProv() {
+    getApi();
+  }
 
-final url = 'https://jsonplaceholder.typicode.com/posts';
-  getApi()async{
+  List<ModelClass> finalList = [];
+
+  final url = 'https://jsonplaceholder.typicode.com/posts';
+  getApi() async {
     Response response = await Dio().get(url);
     final jsonData = response.data;
     final result = ModelClass.fromJson(jsonData);
-    return result;
-
-
-
-
-
+    finalList.add(result);
   }
 }
